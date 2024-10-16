bae1534983ec9232e794917c1430e78c9de568e3
Fredy Augusto Castellon Bedoya <fredyc@readyfit.com>
2024-10-16 16:32:45 -0600

    feat: blocks markdown

---
+++ src/blocks_markdown.py
@@ -0,0 +1,152 @@
+import re
+from enum import Enum
+from parentnode import ParentNode
+from leafnode import LeafNode
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_node_to_html_node
+)
+from inline_markdown import text_to_textnodes
+
+BlockType = Enum("BlockType", ["paragraph", "heading", "code", "quote", "unordered_list", "ordered_list"])
+
+def markdown_to_blocks(markdown):
+    segments = markdown.split('\n\n')
+    blocks = [segment.strip() for segment in segments if len(segment.strip()) > 0]
+    return blocks
+
+def block_to_block_type(block):
+    heading_pattern = r"^#{1,6} .+"
+    if re.match(heading_pattern, block):
+        return BlockType.heading
+    
+    code_pattern = r"```[\s\S]*?```$"
+    if re.match(code_pattern, block):
+        return BlockType.code
+    
+    lines = block.split('\n')
+    
+    quote_pattern = r"^>.*"
+    ok = True
+    for line in lines:
+        if not re.match(quote_pattern, line.strip()):
+            ok = False
+    if ok:
+        return BlockType.quote
+
+
+    unordered_list_pattern = r"^[*-] .+"
+    ok = True
+    for line in lines:
+        if not re.match(unordered_list_pattern, line.strip()):
+            ok = False
+    if ok:
+        return BlockType.unordered_list
+    
+    ordered_list_pattern = r"^\d+\. .+"
+    ok = True
+
+    for line in lines:
+        if not re.match(ordered_list_pattern, line.strip()):
+            ok = False 
+    
+    for i in range(len(lines)):
+        if f"{i+1}" != lines[i].strip()[0]:
+            ok = False
+
+    if ok:
+        return BlockType.ordered_list
+
+    return BlockType.paragraph
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+
+    html_nodes = []
+
+    for block in blocks:
+        block_type = block_to_block_type(block)
+
+        match block_type:
+            case BlockType.heading: 
+                html_nodes.append(markdown_heading_to_html_node(block))
+            case BlockType.code:
+                html_nodes.append(markdown_code_to_html_node(block))
+            case BlockType.quote:
+                html_nodes.append(markdown_quote_to_html_node(block))
+            case BlockType.unordered_list:
+                html_nodes.append(markdown_unordered_list_to_html_node(block))
+            case BlockType.ordered_list:
+                html_nodes.append(markdown_ordered_list_to_html_node(block))
+            case BlockType.paragraph:
+                html_nodes.append(markdown_paragraph_to_html_node(block))
+    
+    return ParentNode("div", html_nodes)
+
+def markdown_heading_to_html_node(heading):
+    heading = heading.strip()
+    if heading.startswith("######"):
+        return LeafNode("h6", heading.replace("######", "").strip())
+    if heading.startswith("#####"):
+        return LeafNode("h5", heading.replace("#####", "").strip())
+    if heading.startswith("####"):
+        return LeafNode("h4", heading.replace("####", "").strip())
+    if heading.startswith("###"):
+        return LeafNode("h3", heading.replace("###", "").strip())
+    if heading.startswith("##"):
+        return LeafNode("h2", heading.replace("##", "").strip())
+    if heading.startswith("#"):
+        return LeafNode("h1", heading.replace("#", "").strip())
+
+def markdown_quote_to_html_node(quote):
+    quotes = quote.strip().split("\n")
+    for i in range(len(quotes)):
+        quotes[i] = quotes[i].replace(">", "").strip()
+    return LeafNode("blockquote", " ".join(quotes))
+
+def markdown_code_to_html_node(code):
+    code_lines = code.replace("```", "").strip().split("\n")
+    for i in range(len(code_lines)):
+        code_lines[i] = code_lines[i].strip()
+    return LeafNode("code", "\n".join(code_lines))
+
+def markdown_unordered_list_to_html_node(unordered_list):
+    items = unordered_list.split("\n")
+    list_items = []
+    for item in items:
+        text_nodes = text_to_textnodes(item.strip()[2:].strip())
+        if len(text_nodes) == 1:
+            list_items.append(LeafNode('li', text_nodes[0].text))
+        elif len(text_nodes) > 1:
+            html_nodes = []
+            for tn in text_nodes:
+                html_nodes.append(text_node_to_html_node(tn))
+            list_items.append(ParentNode('li', html_nodes))
+
+    return ParentNode('ul', list_items)
+
+def markdown_ordered_list_to_html_node(ordered_list):
+    items = ordered_list.split("\n")
+    list_items = []
+    for item in items:
+        text_nodes = text_to_textnodes(item.strip()[2:].strip())
+        if len(text_nodes) == 1:
+            list_items.append(LeafNode('li', text_nodes[0].text))
+        elif len(text_nodes) > 1:
+            html_nodes = []
+            for tn in text_nodes:
+                html_nodes.append(text_node_to_html_node(tn))
+            list_items.append(ParentNode('li', html_nodes))
+
+    return ParentNode('ol', list_items)
+
+def markdown_paragraph_to_html_node(paragraph):
+    text_nodes = text_to_textnodes(" ".join(paragraph.split('\n')))
+    if len(text_nodes) == 1:
+        return LeafNode('p', paragraph)
+    elif len(text_nodes) > 1:
+        html_nodes = []
+        for tn in text_nodes:
+            html_nodes.append(text_node_to_html_node(tn))
+        return ParentNode('p', html_nodes)
---
+++ src/test_block_markdown.py
@@ -0,0 +1,292 @@
+import unittest
+
+from blocks_markdown import (
+    BlockType,
+    markdown_to_blocks,
+    block_to_block_type,
+    markdown_to_html_node
+)
+from leafnode import LeafNode
+
+class BlocksMarkdownTests(unittest.TestCase):
+    def test_markdown_to_blocks(self):
+        markdown = """# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+"""
+        blocks = markdown_to_blocks(markdown)
+        self.assertListEqual(
+            [
+                "# This is a heading",
+                "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+                """* This is the first list item in a list block
+* This is a list item
+* This is another list item"""
+            ],
+            blocks
+        )
+    
+    def test_markdown_to_blocks_with_whitespace(self):
+        markdown = """        # This is a heading               
+
+        This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+        * This is the first list item in a list block
+* This is a list item
+* This is another list item 
+        """
+        blocks = markdown_to_blocks(markdown)
+        self.assertListEqual(
+            [
+                "# This is a heading",
+                "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+                """* This is the first list item in a list block
+* This is a list item
+* This is another list item"""
+            ],
+            blocks
+        )
+
+    def test_markdown_to_blocks_with_empty_blocks(self):
+        markdown = """        # This is a heading               
+
+        This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+        * This is the first list item in a list block
+* This is a list item
+* This is another list item
+
+
+
+
+        """
+        blocks = markdown_to_blocks(markdown)
+        self.assertListEqual(
+            [
+                "# This is a heading",
+                "This is a paragraph of text. It has some **bold** and *italic* words inside of it.",
+                """* This is the first list item in a list block
+* This is a list item
+* This is another list item"""
+            ],
+            blocks
+        )
+
+    def test_block_to_block_type_headings(self):
+        block_heading_1 = "# This is a heading"
+        block_heading_2 = "## This is a heading"
+        block_heading_3 = "### This is a heading"
+        block_heading_4 = "#### This is a heading"
+        block_heading_5 = "##### This is a heading"
+        block_heading_6 = "###### This is a heading"
+        block_not_heading = "This is not a heading"
+
+        block_type = block_to_block_type(block_heading_1)
+        self.assertEqual(BlockType.heading, block_type)
+
+        block_type = block_to_block_type(block_not_heading)
+        self.assertEqual(BlockType.paragraph, block_type)
+
+        block_type = block_to_block_type(block_heading_2)
+        self.assertEqual(BlockType.heading, block_type)
+
+        block_type = block_to_block_type(block_heading_3)
+        self.assertEqual(BlockType.heading, block_type)
+
+        block_type = block_to_block_type(block_not_heading)
+        self.assertEqual(BlockType.paragraph, block_type)
+
+        block_type = block_to_block_type(block_heading_4)
+        self.assertEqual(BlockType.heading, block_type)
+
+        block_type = block_to_block_type(block_heading_5)
+        self.assertEqual(BlockType.heading, block_type)
+
+        block_type = block_to_block_type(block_not_heading)
+        self.assertEqual(BlockType.paragraph, block_type)
+
+        block_type = block_to_block_type(block_heading_6)
+        self.assertEqual(BlockType.heading, block_type)
+
+    def test_block_to_block_type_code(self):
+        block = """```
+        some code
+        ```"""
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.code, block_type)
+
+        block="```some code ```"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.code, block_type)
+
+        block = "```some code ```\nthis is not code"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.paragraph, block_type)
+
+    def test_block_to_block_type_quote(self):
+        block = "> quote"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.quote, block_type)
+
+        block = """> quote
+        > quote
+        > quote""" 
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.quote, block_type)
+
+        block = """> quote
+        > quote
+        > quote
+        not a block quote""" 
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.paragraph, block_type)
+
+    def test_block_to_block_type_unordered_list(self):
+        block = "* list item"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.unordered_list, block_type)
+
+        block = """* item
+        - item
+        * item""" 
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.unordered_list, block_type)
+
+        block = """- item
+        - item
+        - item
+        not an item""" 
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.paragraph, block_type)
+    
+    def test_block_to_block_type_ordered_list(self):
+        block = "1. list item"
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.ordered_list, block_type)
+
+        block = """1. item
+        2. item
+        3. item""" 
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.ordered_list, block_type)
+
+        block = """1. item
+        3. item
+        2. item""" 
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.paragraph, block_type)
+
+        block = """1. item
+        2. item
+        3. item
+        not an item""" 
+        block_type = block_to_block_type(block)
+        self.assertEqual(BlockType.paragraph, block_type)
+
+    def test_markdown_to_html_node(self):
+        markdown = """# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+"""
+        html_node = markdown_to_html_node(markdown)
+
+        self.assertEqual(
+            html_node.to_html(),
+            '<div><h1>This is a heading</h1><p>This is a paragraph of text. It has some <b>bold</b> and <i>italic</i> words inside of it.</p><ul><li>This is the first list item in a list block</li><li>This is a list item</li><li>This is another list item</li></ul></div>'
+        )
+
+    def test_headings(self):
+        md = """
+# this is an h1
+
+this is paragraph text
+
+## this is an h2
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h1>this is an h1</h1><p>this is paragraph text</p><h2>this is an h2</h2></div>",
+        )
+    def test_code(self):
+        md = """
+```this is
+a code
+block
+```
+
+this is paragraph text
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><code>this is\na code\nblock</code><p>this is paragraph text</p></div>",
+        )
+
+    def test_blockquote(self):
+        md = """
+> This is a
+> blockquote block
+
+this is paragraph text
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a blockquote block</blockquote><p>this is paragraph text</p></div>",
+        )
+
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with *italic* text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_lists(self):
+        md = """
+- This is a list
+- with items
+- and *more* items
+
+1. This is an `ordered` list
+2. with items
+3. and more items
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>This is a list</li><li>with items</li><li>and <i>more</i> items</li></ul><ol><li>This is an <code>ordered</code> list</li><li>with items</li><li>and more items</li></ol></div>",
+        )
+
+if __name__ == "__main__":
+    unittest.main()
